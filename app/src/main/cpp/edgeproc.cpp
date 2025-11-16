5327da3796ef2a00fc20a5a452671cf6075460dd
Rudraksh-Agnihotri4 <rudrakshagnihotri45@gmail.com>
2025-11-16 18:28:04 +0530

    android: add manifest + GLSurfaceView layout + remove compose UI

---
+++ app/src/main/cpp/edgeproc.cpp
@@ -0,0 +1,10 @@
+#include <jni.h>
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_rudviwer_MainActivity_dummyNative(
+        JNIEnv* env,
+        jobject thiz
+) {
+    // Temporary empty native function
+}
